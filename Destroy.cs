f45b62cff8fd934a8fe7570c50cbfab0036c0fd3
Taty5552 <118651490+Taty5552@users.noreply.github.com>
2022-11-20 15:36:48 +0500

    Add files via upload

---
+++ Destroy.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject);
+        // Уничтожение объекта
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+}
